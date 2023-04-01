995e1465981a7d9e9ab84aef9e43d971a42b8f01
unknown <leonardohchoi@gmail.com>
2023-04-01 19:04:11 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "./components/Header";
+import FeedbackItem from "./components/FeedbackItem";
+import FeedbackData from "./data/FeedbackData";
+import { useState } from "react";
+
+function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
+
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <FeedbackItem />
+      </div>
+    </>
+  );
+}
+
+export default App;
