b70cfd7bcc5a19310285c24eb2f5640c818c15ee
unknown <leonardohchoi@gmail.com>
2023-04-07 16:48:40 -0300

    add RatingSelect component

---
+++ src/components/RatingSelect.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useState } from "react";
+
+function RatingSelect() {
+  const [selected, setSelected] = useState();
+  return <div>Rating Select</div>;
+}
+
+export default RatingSelect;
