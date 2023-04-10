d9bb87b1051cf500a8804ff331199fa9d203aa3a
unknown <leonardohchoi@gmail.com>
2023-04-10 08:57:04 -0300

    add animation to feedbackList

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
+import { v4 as uuidv4 } from "uuid";
 import React from "react";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackData from "./data/FeedbackData";
 import { useState } from "react";
 import FeedbackStats from "./components/FeedbackStats";
-import FeedbackForm from "./components/Form";
+import Form from "./components/Form";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
+  };
 
   const deleteFeedback = (id) => {
     if (window.confirm("are you sure you want to delete?")) {
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
-
-  const addFeedback = (newFeedback) => {};
 
   return (
     <>
